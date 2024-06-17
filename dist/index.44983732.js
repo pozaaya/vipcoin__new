859cbfd4dca2db806a39d7daf88bbcb913ebc9d5
Polina <polina.zayatsss@gmail.com>
2024-06-18 01:19:30 +0300

    first commit

---
+++ dist/index.44983732.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper(".swiper-container", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 20,
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    // And if we need scrollbar
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        draggable: true
+    }
+});
+
+//# sourceMappingURL=index.44983732.js.map
